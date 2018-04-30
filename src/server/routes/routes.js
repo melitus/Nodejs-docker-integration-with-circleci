db5675253ce93f9958358693e302611edb66a5d3
melitus <asmelitus@gmail.com>
2018-04-30 18:13:27 +0200

    Express server set up with test route

---
+++ src/server/routes/routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+/**
+ * GET home page
+ */
+routes.get('/', (req, res) => {
+  res.render('index', { title: 'Express Babel' });
+});
+export default routes;
